da4d94ec5ae138d6b8759b6bf3c0264232b12569
ab6ul3ziz <123942600+ab6ul3ziz@users.noreply.github.com>
2025-01-11 16:55:53 +0200

    update things

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseApi = axios.create({
-  baseURL: "http://localhost:2000/api",
+  baseURL: "https://albwaba-server.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
